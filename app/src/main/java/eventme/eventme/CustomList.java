656373ae1e1b52fbae1291a8e462888e566a7af0
Karol <karolidist@gmail.com>
2017-06-29 16:50:56 +0300

    update homepage

--- app/src/main/java/eventme/eventme/CustomList.java
+++ app/src/main/java/eventme/eventme/CustomList.java
@@ -4,13 +4,9 @@ package eventme.eventme;
  */
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
